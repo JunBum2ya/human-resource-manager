a1c1b379b46aa58be522bd666bd4eccc3c715de3
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-04-01 16:57:27 +0900

    [feat] init project
    spring project 생성

---
+++ src/main/java/com/ecorich/hrservice/HrmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.ecorich.hrservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrmanagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecorich/hrservice/HrmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecorich.hrservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
