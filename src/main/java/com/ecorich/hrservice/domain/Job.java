9eb1910f41f05b85f51aa383d9aca421ec9c6187
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-04-02 10:15:59 +0900

    [feat] job domain
    업무 도메인 추가

---
+++ src/main/java/com/ecorich/hrservice/domain/Job.java
@@ -0,0 +1,18 @@
+package com.ecorich.hrservice.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table(name = "jobs")
+@Entity
+public class Job {
+    @Id
+    @Column(name = "job_id", nullable = false, length = 10)
+    private String id;
+    @Column(name = "job_title", nullable = false, length = 35)
+    private String title;
+    private Long minSalary;
+    private Long maxSalary;
+}
