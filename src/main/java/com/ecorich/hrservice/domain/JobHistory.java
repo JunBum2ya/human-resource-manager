c86418c0cf00dae7cff0c6abcc21f2247ab447ae
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-04-02 11:06:27 +0900

    [feat] job history domain
     작업 내역 도메인 추가

---
+++ src/main/java/com/ecorich/hrservice/domain/JobHistory.java
@@ -0,0 +1,27 @@
+package com.ecorich.hrservice.domain;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Table(name = "job_history", indexes = {@Index(columnList = "employee_id"), @Index(columnList = "start_date")})
+@Entity
+public class JobHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "history_id", nullable = false)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "employee_id")
+    private Employee employee;
+    @Column(nullable = false)
+    private LocalDate startDate;
+    @Column(nullable = false)
+    private LocalDate endDate;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "job_id")
+    private Job job;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "department_id")
+    private Department department;
+}
