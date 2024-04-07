b3f382121455a7c633b189a19a4622bd39723fbd
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-04-07 18:20:37 +0900

    [fix] location api response
    위치 응답 포맷 수정

--- src/main/java/com/ecorich/hrservice/dto/response/DepartmentDetailResponse.java
+++ src/main/java/com/ecorich/hrservice/dto/response/DepartmentDetailResponse.java
@@ -7,14 +7,14 @@ import lombok.Builder;
 @Schema(name = "부서 상세 응답 포맷", description = "부서 상세 응답 데이터 양식")
 @Builder
 public record DepartmentDetailResponse(Long departmentId, String departmentName, EmployeeResponse manager,
-                                       LocationResponse location) {
+                                       LocationDetailResponse location) {
 
     public static DepartmentDetailResponse from(DepartmentDetailData department) {
         return DepartmentDetailResponse.builder()
                 .departmentId(department.departmentId())
                 .departmentName(department.departmentName())
                 .manager(department.manager() != null ? EmployeeResponse.from(department.manager()) : null)
-                .location(department.manager() != null ? LocationResponse.from(department.location()) : null)
+                .location(department.manager() != null ? LocationDetailResponse.from(department.location()) : null)
                 .build();
     }
 }
