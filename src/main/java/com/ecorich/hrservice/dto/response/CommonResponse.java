a2b9d319cd5a20cee6140048eb28bdddc41088a8
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-04-07 22:01:49 +0900

    [fix] add schema
    스키마 추가

--- src/main/java/com/ecorich/hrservice/dto/response/CommonResponse.java
+++ src/main/java/com/ecorich/hrservice/dto/response/CommonResponse.java
@@ -2,9 +2,15 @@ package com.ecorich.hrservice.dto.response;
 
 import com.ecorich.hrservice.domain.constant.ResultStatus;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(name = "공통 응답 포맷", description = "성공 여부를 보여주는 공통 응답 포맷")
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public record CommonResponse<T>(String code, String message, T data) {
+public record CommonResponse<T>(
+        @Schema(name = "code", description = "응답코드") String code,
+        @Schema(name = "message", description = "응답 메시지") String message,
+        @Schema(name = "data", description = "데이터") T data
+) {
     public static<T> CommonResponse<T> of(ResultStatus status, T data) {
         return new CommonResponse<T>(status.getCode(), status.getMessage(), data);
     }
